f897bd044e8661fcee8c6e0e54b5b3868574cbbb
Strelka <pashastrelyaev@yandex.ru>
2025-03-24 17:35:59 +0300

    Скрипт

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Pasha487/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const cardDeleteButton = card.querySelector('.card__delete-button');
+
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    cardDeleteButton.addEventListener('click', () => {
+        deleteCard(card);
+    });
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => {
+    const card = createCard(cardData, deleteCard);
+    placesList.append(card);
+});
