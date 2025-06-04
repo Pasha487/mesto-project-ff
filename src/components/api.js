5bd7553a9a7bb578626a8eb32983f85d0945bc84
Strelka <pashastrelyaev@yandex.ru>
2025-06-04 12:43:23 +0300

    добавление API

---
+++ src/components/api.js
@@ -0,0 +1,7 @@
+const config = {
+    baseUrl: 'https://mesto.nomoreparties.co/v1/wff-cohort-39',
+    headers: {
+        authorization: '4a21a4d8-63db-4c80-9c68-44a6c92c99ff',
+        'content-type': 'application/json'
+    }
+}
